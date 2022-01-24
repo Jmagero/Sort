23c8e9a5294a22ee099329def407a71b3ae92313
Jocyline Magero <jmagero54@gmail.com>
2022-01-24 17:45:45 -0600

    bubble sort implementation

---
+++ bubbleSort.java
@@ -0,0 +1,12 @@
+public static int[] bubbleSort(int [] arr){
+    for(int i= 0; i<arr.length; i++){
+      for(int j= i+1; j< arr.length; j++){
+        if(arr[i] > arr[j]){
+          int temp = arr[i];
+          arr[i] = arr[j];
+          arr[j] = temp;
+        }
+      }
+    }
+    return arr;
+}
